06dbfe63358a36bb68384724448443c9bec61d26
Lu MingXin <17839993500@163.com>
2021-06-04 14:26:24 +0800

    part3

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
 // 弹出侧边栏
 $('#btn').on('click',function(){
 	if($('.page').width()>=480) {
-		$('.page').animate({'margin-left': '-390px'})
+		// $('.page').animate({'margin-left': '-390px'},{easing: 'linear'})
+		$('.page').addClass('side-bar-in')
+		$('.page').removeClass('side-bar-out')
+
 	}else {
-		$('.page').animate({'margin-left': '-375px'})
+		$('.page').animate({'margin-left': '-375px'},{easing: 'linear'})
+
 	}
 })
 // 隐藏侧边栏
 $('.close').on('click',function(){
-	$('.page').animate({'margin-left': '0'})
+	// $('.page').animate({'margin-left': '0'},{easing: 'linear'})
+	$('.page').addClass('side-bar-out')
+	$('.page').removeClass('side-bar-in')
 })
