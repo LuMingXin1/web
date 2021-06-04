2558cc42fbfa5786439366574733ac6436811d92
Lu MingXin <17839993500@163.com>
2021-06-04 15:50:11 +0800

    fix-lang

--- js/lang/language.js
+++ js/lang/language.js
@@ -11,10 +11,14 @@ function changeLang(lang) {
     cache: false,
     mode: 'map', //用Map的方式使用资源文件中的值
     callback: function () {
-      //加载成功后设置显示内容
+      //加载成功后设置显示内容  替换文本
       for (let key in $.i18n.map) {
         $('[data-lang="' + key + '"]').text($.i18n.map[key]);
       }
+      // 替换图片
+      $('.switch').each((_,item)=>{
+        $(item).attr('src',$(item).attr(lang))
+      })
       document.title = $.i18n.map['title'];
     },
   });
