925398d2290ed016f94c2823482e3b748e0b73bc
Lu MingXin <17839993500@163.com>
2021-06-08 16:22:54 +0800

    side-bar

--- js/index.js
+++ js/index.js
@@ -1,18 +1,10 @@
 // 弹出侧边栏
 $('#btn').on('click',function(){
-	if($('.page').width()>=480) {
-		// $('.page').animate({'margin-left': '-390px'},{easing: 'linear'})
-		$('.page').addClass('side-bar-in')
-		$('.page').removeClass('side-bar-out')
-
-	}else {
-		// $('.page').animate({'margin-left': '-375px'},{easing: 'linear'})
-		console.log('小屏幕');
-	}
+	$('.page').addClass('side-bar-in')
+	$('.page').removeClass('side-bar-out')
 })
 // 隐藏侧边栏
 $('.close').on('click',function(){
-	// $('.page').animate({'margin-left': '0'},{easing: 'linear'})
 	$('.page').addClass('side-bar-out')
 	$('.page').removeClass('side-bar-in')
 })
