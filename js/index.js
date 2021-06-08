e4cca1ea0ac2e629be7ba4ae86c98342c4e3c5a0
Lu MingXin <17839993500@163.com>
2021-06-08 17:03:05 +0800

    side-bar-inner

--- js/index.js
+++ js/index.js
@@ -4,7 +4,18 @@ $('#btn').on('click',function(){
 	$('.page').removeClass('side-bar-out')
 })
 // 隐藏侧边栏
-$('.close').on('click',function(){
+$('.side-bar-close').on('click',function(){
 	$('.page').addClass('side-bar-out')
 	$('.page').removeClass('side-bar-in')
+})
+// 更改语言
+$('.lang-zh').on('click',function(){
+	changeLang('zh')
+})
+$('.lang-en').on('click',function(){
+	changeLang('en')
+})
+// 跳转功能介绍
+$('#toFunctionIntroduction').on('click',function(){
+	
 })
